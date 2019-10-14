e2648022fbc2a136e2dc3fe5a5e7c34cc7661ee0
Wmbat <wmbat@protonmail.com>
2019-10-14 17:30:11 -0400

    Removing build files

--- build/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wmbat/Documents/programming/cpp/wmbats_bazaar/external/googletest/googletest
-# Build directory: /home/wmbat/Documents/programming/cpp/wmbats_bazaar/build/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
